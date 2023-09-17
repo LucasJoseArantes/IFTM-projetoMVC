fe0e66b1539843b6a19be8643bf98ed9ff18419e
Lucas Jose <lucas.arnts@outlook.com>
2023-09-17 12:42:40 -0300

    update edit

--- ex_newsletter/newsletter/src/main/java/lucas/jose/br/com/newsletter/dto/CadastroDto.java
+++ ex_newsletter/newsletter/src/main/java/lucas/jose/br/com/newsletter/dto/CadastroDto.java
@@ -7,9 +7,4 @@ public class CadastroDto {
     private String name;
     private String email;
     private String number;
-
-    
-    private String nameModel;
-    private String emailModel;
-    private String numberModel;
 }
--- ex_newsletter/newsletter/src/main/java/lucas/jose/br/com/newsletter/resources/CadastroResource.java
+++ ex_newsletter/newsletter/src/main/java/lucas/jose/br/com/newsletter/resources/CadastroResource.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 import lucas.jose.br.com.newsletter.dto.CadastroDto;
 
 @Controller
@@ -36,33 +40,6 @@ public class CadastroResource {
             }
         }
 
-        return doGet(model);
-    }
-
-    @RequestMapping("cadastroUpdate")
-    public String editForm(CadastroDto dto, String name, Model model) {
-        // Primeiro, encontre o cadastro a ser atualizado
-        CadastroDto cadastroParaAtualizar = null;
-        for (CadastroDto cadastro : cadastros) {
-            if (cadastro.getName().equals(name)) {
-                cadastroParaAtualizar = cadastro;
-                break;
-            }
-        }
-    
-        // Se encontrou o cadastro, remova-o da lista
-        if (cadastroParaAtualizar != null) {
-            cadastros.remove(cadastroParaAtualizar);
-    
-            // Atualize os valores do cadastro com os valores do DTO
-            cadastroParaAtualizar.setName(dto.getNameModel());
-            cadastroParaAtualizar.setEmail(dto.getEmailModel());
-            cadastroParaAtualizar.setNumber(dto.getNumberModel());
-    
-            // Adicione o cadastro atualizado de volta à lista
-            cadastros.add(cadastroParaAtualizar);
-        }
-    
         return doGet(model);
     }
     
